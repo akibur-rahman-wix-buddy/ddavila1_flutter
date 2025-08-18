e01a13817267b847ab640388ffa9c1328b0140ee
shoriful islam <sholifulislamemon@gmail.com>
2025-08-18 11:59:41 +0600

    solve the filter section

--- lib/navigation_screen.dart
+++ lib/navigation_screen.dart
@@ -1,31 +1,263 @@
+// // ignore_for_file: unused_element, library_private_types_in_public_api
+// import 'package:ddavila/assets_helper/app_colors.dart';
+// import 'package:ddavila/features/chat/presentation/chat_screen.dart';
+// import 'package:ddavila/features/user_app/home_screen/presentation/home_screen.dart';
+// import 'package:ddavila/features/user_app/products_screen/products_screen.dart';
+// import 'package:ddavila/features/user_app/profile_screen/presentation/profile_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'assets_helper/app_icons.dart';
+//
+// class NavigationScreen extends StatefulWidget {
+//   const NavigationScreen({super.key});
+//
+//   @override
+//   _NavigationScreenState createState() => _NavigationScreenState();
+// }
+//
+// class _NavigationScreenState extends State<NavigationScreen> {
+//   bool showOverlay = true;
+//   int selectedIndex = 0;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+//
+//   // * Screen List
+//   final List<Widget> _screens = [
+//     const HomeScreen(),
+//     const ProductsScreen(),
+//     const ChatScreen(),
+//     const ProfileScreen(),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         SafeArea(
+//           child: Scaffold(
+//             bottomNavigationBar: Stack(
+//               alignment: Alignment.bottomCenter,
+//               children: [
+//                 IndexedStack(
+//                   index: selectedIndex, // This tells which screen to show
+//                   children: _screens,
+//                 ),
+//                 Positioned(
+//                   bottom: 30.h,
+//                   left: 30.w,
+//                   right: 30.w,
+//                   child: Container(
+//                     height: 74.h,
+//                     width: 310.w,
+//                     decoration: BoxDecoration(
+//                       color: AppColor.c3988FF,
+//                       borderRadius: BorderRadius.circular(67.r),
+//                     ),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         _buildNavItem(AppIcons.navHome, 0),
+//                         _buildNavItem(AppIcons.navNote, 1),
+//                         _buildNavItem(AppIcons.navMessage, 2),
+//                         _buildNavItem(AppIcons.navProfile, 3),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+//
+//   Widget _buildNavItem(String icon, int index) {
+//     bool isSelected = selectedIndex == index;
+//
+//     return GestureDetector(
+//       onTap: () {
+//         setState(() {
+//           selectedIndex = index;
+//         });
+//       },
+//       child: Container(
+//         height: 50.h,
+//         width: 50.h,
+//         decoration: BoxDecoration(
+//           color: isSelected ? Colors.white : Colors.transparent,
+//           shape: BoxShape.circle, // শুধু circle, কোনো borderRadius নেই
+//         ),
+//         child: Center(
+//           child: SvgPicture.asset(
+//             icon,
+//             colorFilter: ColorFilter.mode(
+//               isSelected ? AppColor.c3988FF : AppColor.cFFFFFF,
+//               BlendMode.srcIn,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// extension on Color {
+//   withValues({required double alpha}) {}
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///>>>>>>>>>>>>>>>>>> zobayer vai new code >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
 // ignore_for_file: unused_element, library_private_types_in_public_api
 import 'package:ddavila/assets_helper/app_colors.dart';
+import 'package:ddavila/assets_helper/text_font_style.dart';
 import 'package:ddavila/features/chat/presentation/chat_screen.dart';
 import 'package:ddavila/features/user_app/home_screen/presentation/home_screen.dart';
 import 'package:ddavila/features/user_app/products_screen/products_screen.dart';
 import 'package:ddavila/features/user_app/profile_screen/presentation/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'assets_helper/app_icons.dart';
 
+
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
+
 
   @override
   _NavigationScreenState createState() => _NavigationScreenState();
 }
 
+
+// class _NavigationScreenState extends State<NavigationScreen> {
+//   bool showOverlay = true;
+//   int selectedIndex = 0;
+
+
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+
+//   // * Screen List
+//   final List<Widget> _screens = [
+//     const HomeScreen(),
+//     const ProductsScreen(),
+//     const ChatScreen(),
+//     const ProfileScreen(),
+//   ];
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         SafeArea(
+//           child: Scaffold(
+//             bottomNavigationBar: Stack(
+//               alignment: Alignment.bottomCenter,
+//               children: [
+//                 IndexedStack(
+//                   index: selectedIndex, // This tells which screen to show
+//                   children: _screens,
+//                 ),
+//                 Positioned(
+//                   bottom: 30.h,
+//                   left: 30.w,
+//                   right: 30.w,
+//                   child: Container(
+//                     height: 74.h,
+//                     width: 310.w,
+//                     decoration: BoxDecoration(
+//                       color: AppColor.c3988FF,
+//                       borderRadius: BorderRadius.circular(67.r),
+//                     ),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         _buildNavItem(AppIcons.navHome, 0),
+//                         _buildNavItem(AppIcons.navNote, 1),
+//                         _buildNavItem(AppIcons.navMessage, 2),
+//                         _buildNavItem(AppIcons.navProfile, 3),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             floatingActionButton: FloatingActionButton(
+//               onPressed: () {
+//                 // Handle floating action button press
+//               },
+//               child: const Icon(Icons.add),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+
+
+//   Widget _buildNavItem(String icon, int index) {
+//     bool isSelected = selectedIndex == index;
+
+
+//     return GestureDetector(
+//       onTap: () {
+//         setState(() {
+//           selectedIndex = index;
+//         });
+//       },
+//       child: Container(
+//         height: 50.h,
+//         width: 50.h,
+//         decoration: BoxDecoration(
+//           color: isSelected ? Colors.white : Colors.transparent,
+//           shape: BoxShape.circle, // শুধু circle, কোনো borderRadius নেই
+//         ),
+//         child: Center(
+//           child: SvgPicture.asset(
+//             icon,
+//             colorFilter: ColorFilter.mode(
+//               isSelected ? AppColor.c3988FF : AppColor.cFFFFFF,
+//               BlendMode.srcIn,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 class _NavigationScreenState extends State<NavigationScreen> {
-  bool showOverlay = true;
   int selectedIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-  }
 
-  // * Screen List
   final List<Widget> _screens = [
     const HomeScreen(),
     const ProductsScreen(),
@@ -33,51 +265,264 @@ class _NavigationScreenState extends State<NavigationScreen> {
     const ProfileScreen(),
   ];
 
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SafeArea(
-          child: Scaffold(
-            bottomNavigationBar: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                IndexedStack(
-                  index: selectedIndex, // This tells which screen to show
-                  children: _screens,
-                ),
-                Positioned(
-                  bottom: 30.h,
-                  left: 30.w,
-                  right: 30.w,
-                  child: Container(
-                    height: 74.h,
-                    width: 310.w,
-                    decoration: BoxDecoration(
-                      color: AppColor.c3988FF,
-                      borderRadius: BorderRadius.circular(67.r),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        _buildNavItem(AppIcons.navHome, 0),
-                        _buildNavItem(AppIcons.navNote, 1),
-                        _buildNavItem(AppIcons.navMessage, 2),
-                        _buildNavItem(AppIcons.navProfile, 3),
-                      ],
+    return Scaffold(
+      body: IndexedStack(
+        index: selectedIndex,
+        children: _screens,
+      ),
+
+
+      // ✅ ExpandableFab ঠিকভাবে কাজ করবে
+      floatingActionButton: ExpandableFab(
+        type: ExpandableFabType.up,
+        distance: 70,
+        childrenAnimation: ExpandableFabAnimation.none,
+        overlayStyle: ExpandableFabOverlayStyle(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        openButtonBuilder: RotateFloatingActionButtonBuilder(
+          child: SvgPicture.asset(
+            AppIcons.addIcon,
+          ),
+          fabSize: ExpandableFabSize.regular,
+          backgroundColor: Colors.white,
+        ),
+        closeButtonBuilder: RotateFloatingActionButtonBuilder(
+          child: SvgPicture.asset(
+            AppIcons.crossIcon,
+          ),
+          fabSize: ExpandableFabSize.regular,
+          backgroundColor: Colors.white,
+        ),
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: AppColor.cFFFFFF,
+              borderRadius: BorderRadius.circular(99.r),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  const SizedBox(width: 6),
+                  Text(
+                    'Buying Order',
+                    style: TextFontStyle.textLine7w400cFFFFFFDmSans.copyWith(
+                      color: AppColor.c000000,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(width: 10),
+                  FloatingActionButton.small(
+                    heroTag: 'remind',
+                    backgroundColor: AppColor.cFFFFFF,
+                    foregroundColor: AppColor.cFFFFFF,
+                    disabledElevation: 0,
+                    elevation: 0,
+                    onPressed: () {
+                      debugPrint("Buying Order");
+                    },
+                    child: SvgPicture.asset(AppIcons.orderIcon),
+                  ),
+                ],
+              ),
             ),
           ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColor.cFFFFFF,
+              borderRadius: BorderRadius.circular(99.r),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  const SizedBox(width: 10),
+                  Text(
+                    'Selling Order',
+                    style: TextFontStyle.textLine7w400cFFFFFFDmSans.copyWith(
+                      color: AppColor.c000000,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  FloatingActionButton.small(
+                    heroTag: 'remind',
+                    backgroundColor: AppColor.cFFFFFF,
+                    foregroundColor: AppColor.cFFFFFF,
+                    disabledElevation: 0,
+                    elevation: 0,
+                    onPressed: () {
+                      debugPrint("Selling Order");
+                    },
+                    child: SvgPicture.asset(AppIcons.orderIcon),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColor.cFFFFFF,
+              borderRadius: BorderRadius.circular(99.r),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  const SizedBox(width: 40),
+                  Text(
+                    'Whitelist',
+                    style: TextFontStyle.textLine7w400cFFFFFFDmSans.copyWith(
+                      color: AppColor.c000000,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  FloatingActionButton.small(
+                    heroTag: 'remind',
+                    backgroundColor: AppColor.cFFFFFF,
+                    foregroundColor: AppColor.cFFFFFF,
+                    disabledElevation: 0,
+                    elevation: 0,
+                    onPressed: () {
+                      debugPrint("Whitelist");
+                    },
+                    child: SvgPicture.asset(AppIcons.orderIcon),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColor.cFFFFFF,
+              borderRadius: BorderRadius.circular(99.r),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  const SizedBox(width: 25),
+                  Text(
+                    'Bid History',
+                    style: TextFontStyle.textLine7w400cFFFFFFDmSans.copyWith(
+                      color: AppColor.c000000,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  FloatingActionButton.small(
+                    heroTag: 'remind',
+                    backgroundColor: AppColor.cFFFFFF,
+                    foregroundColor: AppColor.cFFFFFF,
+                    disabledElevation: 0,
+                    elevation: 0,
+                    onPressed: () {
+                      debugPrint("Bid History");
+                    },
+                    child: SvgPicture.asset(AppIcons.orderIcon),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColor.cFFFFFF,
+              borderRadius: BorderRadius.circular(99.r),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  const SizedBox(width: 23),
+                  Text(
+                    'My Auction',
+                    style: TextFontStyle.textLine7w400cFFFFFFDmSans.copyWith(
+                      color: AppColor.c000000,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  FloatingActionButton.small(
+                    heroTag: 'remind',
+                    backgroundColor: AppColor.cFFFFFF,
+                    foregroundColor: AppColor.cFFFFFF,
+                    disabledElevation: 0,
+                    elevation: 0,
+                    onPressed: () {
+                      debugPrint("My Auction");
+                    },
+                    child: SvgPicture.asset(AppIcons.orderIcon),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColor.cFFFFFF,
+              borderRadius: BorderRadius.circular(99.r),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  const SizedBox(width: 23),
+                  Text(
+                    'Dashboard',
+                    style: TextFontStyle.textLine7w400cFFFFFFDmSans.copyWith(
+                      color: AppColor.c000000,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  FloatingActionButton.small(
+                    heroTag: 'remind',
+                    backgroundColor: AppColor.cFFFFFF,
+                    foregroundColor: AppColor.cFFFFFF,
+                    disabledElevation: 0,
+                    elevation: 0,
+                    onPressed: () {
+                      debugPrint("Dashboard");
+                    },
+                    child: SvgPicture.asset(AppIcons.orderIcon),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: ExpandableFab.location,
+
+
+      // ✅ custom bottom navigation
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(bottom: 20.h, left: 30.w, right: 30.w),
+        child: Container(
+          height: 74.h,
+          width: 310.w,
+          decoration: BoxDecoration(
+            color: AppColor.c3988FF,
+            borderRadius: BorderRadius.circular(67.r),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildNavItem(AppIcons.navHome, 0),
+              _buildNavItem(AppIcons.navNote, 1),
+              _buildNavItem(AppIcons.navMessage, 2),
+              _buildNavItem(AppIcons.navProfile, 3),
+            ],
+          ),
         ),
-      ],
+      ),
     );
   }
 
+
   Widget _buildNavItem(String icon, int index) {
     bool isSelected = selectedIndex == index;
+
 
     return GestureDetector(
       onTap: () {
@@ -90,7 +535,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
         width: 50.h,
         decoration: BoxDecoration(
           color: isSelected ? Colors.white : Colors.transparent,
-          shape: BoxShape.circle, // শুধু circle, কোনো borderRadius নেই
+          shape: BoxShape.circle,
         ),
         child: Center(
           child: SvgPicture.asset(
@@ -106,6 +551,10 @@ class _NavigationScreenState extends State<NavigationScreen> {
   }
 }
 
+
 extension on Color {
   withValues({required double alpha}) {}
 }
+
+
+
